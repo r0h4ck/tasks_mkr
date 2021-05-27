890db2644e5743634a2a7740fc4cd93219344dd3
r0h4ck <66730695+r0h4ck@users.noreply.github.com>
2021-05-27 13:51:04 +0300

    Create tasks.php

---
+++ tasks.php
@@ -0,0 +1,18 @@
+<?php
+class Autobus
+{
+    public $places, $prod_year;
+    
+    public $coutry = [1 => "Україна", 2 => "Китай", 3 => "Білорусь"];
+
+}
+class Depo
+{
+    
+    public $autobuses = array(1, 2, 3);
+    
+}
+$bmw = new Autobus();
+$bmw -> places >= 0;
+$bmw -> count($counry = 1);
+?>
